5af64d2b4785a0f567742c076aa40ab2a8555225
lijo A J <abrahamjoseph.l@incois.gov.in>
2019-02-26 10:25:53 +0530

    script to pull data from mihir

---
+++ getDataFromMihir.sh
@@ -0,0 +1,49 @@
+getData() {
+  # date="20181008" # for single date
+  date="$1" # pass dates through loop
+  DIR="/incois_ncmrwfx/incois/hycom/dataset/gaja_IMD"
+  # SORC="/home/hycom/IMD_GFS"
+  SORC="/home/imdgfs/shared/data/T1534/pgrb/0p25deg"
+  for hr in {00..18..06}
+  do
+    DEST="${DIR}/gdas.${date}${hr}"
+    if [[ -d "$DEST" ]]
+    then
+      echo "folder exists"
+    else
+      echo "create gdas folder"
+      mkdir -p "$DEST"
+    fi
+    echo "copying file from Mihir ==========> gdas.${date}${hr}"
+  
+    # grib
+    rsync -auvhP mihir:${SORC}/gdas.${date}${hr}/gdas.t${hr}z.pgrb2.0p25.f{000..129..03} $DEST/
+    # grib
+    rsync -auvhP mihir:${SORC}/gdas.${date}${hr}/gdas.t${hr}z.pgrb2.0p25.f{000..006..01} $DEST/
+    # nemsio
+    # rsync -auvhP mihir:${SORC}/gdas.${date}${hr}/gdas1.t${hr}z.sfcanl.nemsio $DEST/
+    # rsync -auvhP mihir:${SORC}/gdas.${date}${hr}/gdas.t${hr}z.atmf000.nemsio $DEST/
+    # rsync -auvhP mihir:${SORC}/gdas.${date}${hr}/gdas.t${hr}z.atmf{003..009..003}.nemsio $DEST/
+    # prepbufr
+    # rsync -auvhP mihir:${SORC}/gdas.${date}${hr}/*t${hr}z*prepbufr* $DEST/
+    # bias
+    # rsync -auvhP mihir:${SORC}/gdas.${date}${hr}/*t${hr}z*bias* $DEST/
+  
+    # bufr_d
+    # for f in gpsro goesnd goesfv 1bhrs2 1bamua 1bamub 1bhrs3 1bhrs4 1bmhs airsev \
+      # sevcsr mtiasi esamua esamub.eshrs3 ssmisu amsre atms cris saphir gmi ssmit \
+      # avcsam avcspm
+    # do
+      # rsync -auvhP mihir:${SORC}/gdas.${date}${hr}/*t${hr}*${f}*.bufr_d $DEST/
+    # done
+  done
+}
+
+dt="20181110"
+fdt="20181120"
+while [[ ${dt} != ${fdt} ]]
+do
+  echo "<======Downloading for date : ${dt}======>"
+  getData ${dt}
+  dt=$(date -d "${dt} +1 day" +"%Y%m%d")
+done
